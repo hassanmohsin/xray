48a3bc06525640406f2e87aab2460cbae1e12907
Mahmudulla Hassan <mhassan@miners.utep.edu>
2021-04-25 18:12:39 -0600

    correct colors

--- xray/config.py
+++ xray/config.py
@@ -8,14 +8,14 @@ class Material:
         self.material_constant = {
             'metal': [.161, .486, .965],  # blue (0, 0, 255)
             'knife': [.957, .749, .478],  # red (255, 0, 0)
-            'plastic': [.318, .741, .506],  # orange(255, 165, 0)
-            'cloth': [.318, .741, .506],  # red orange (255, 69, 0)
-            'leather': [0.9, 0.3, 0.6],  # green (0, 255, 0),
-            'unk': [.318, .741, .506]  # orange(255, 165, 0)
+            'plastic': [.957, .749, .478],  # orange(255, 165, 0)
+            'cloth': [.957, .749, .478],  # red orange (255, 69, 0)
+            'leather': [.318, .741, .506],  # green (0, 255, 0),
+            'unk': [.957, .749, .478]  # orange(255, 165, 0)
         }
 
         # Normalize
-        r = np.array(list(self.material_constant.values()))
+        r = -np.log(np.array(list(self.material_constant.values())))
         r = r / np.sqrt(np.sum(r ** 2, axis=1, keepdims=True))
         self.material_constant = dict(zip(list(self.material_constant.keys()), r))
 
