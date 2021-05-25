02a71e84c8a78a15904a90ccdde04d7a57a02d5d
Mahmudulla Hassan <mhassan@miners.utep.edu>
2021-05-25 03:28:10 -0600

    update link for new set of stl files

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # Download stl files
-wget "https://minersutep-my.sharepoint.com/:u:/g/personal/mhassan_miners_utep_edu/EVQaQ975JBhNlet5g12H8-IBkMcDRuA-f85Qe5Yklf9VSA?e=caze5C&download=1" -O stls.tar.gz
+wget "https://minersutep-my.sharepoint.com/:u:/g/personal/mhassan_miners_utep_edu/EeT8imHzpNFMl73jB35GRn4BzV84EnnfkrcqXzL3Qo7UBA?e=ikFQcv&download=1" -O stls.tar.gz
 tar -xzvf stls.tar.gz && rm -f stls.tar.gz
 
