5f36a312fd203fa302b705eb2b3806dcaa89b0b2
hassanmohsin <mhassan@miners.utep.edu>
2021-06-20 23:16:41 -0600

    update generate_dataset.py

--- xray/generate_dataset.py
+++ xray/generate_dataset.py
@@ -1,6 +1,4 @@
-import json
 import os
-import re
 import shutil
 from argparse import ArgumentParser
 from time import time
@@ -12,34 +10,28 @@ from tqdm import tqdm
 
 def argument_parser():
     parser = ArgumentParser(description='Generate xray image dataset.')
-    parser.add_argument('--input', type=str, required=True, action='store',
-                        help="JSON input")
+    parser.add_argument('--images', type=str, required=True, action='store', help="Image directory")
     parser.add_argument('--set', type=str, required=True, action='store', help="Name of the set e.g., 'train'")
+    parser.add_argument('--split', type=float, required=True, action='store',
+                        help="Ratio of positive to negative samples")
     args = parser.parse_args()
-    if not os.path.isfile(args.input):
-        raise FileNotFoundError("Input {args.input} not found.")
-
-    with open(args.input) as f:
-        image_args = json.load(f)
-
     tic = time()
 
     dest_dir = f"./{args.set}/{args.set}-set"
     if not os.path.isdir(dest_dir):
         os.makedirs(dest_dir)
 
-    images = os.listdir(f"./{args.set}/images/no_ooi")
-    ImageIds = [re.findall("\d+", f)[0] for f in images]
-    ImageIds = [int(i) for i in ImageIds]
-    labels = np.random.randint(2, size=len(ImageIds))
+    images = os.listdir(f"./{args.images}/no_ooi/zview")
+    ImageIds = [os.path.splitext(i)[0] for i in images]
+    labels = np.random.uniform(size=len(ImageIds)) > args.split
 
     for id, label in tqdm(zip(ImageIds, labels), total=len(labels)):
-        src = "ooi" if label == 1 else "no_ooi"
-        src = os.path.join(f"./{args.set}/images", src, f"image-z_{id}.png")
-        dest = os.path.join(dest_dir, f"{id:06}.png")
+        src = "ooi/zview" if label else "no_ooi/zview"
+        src = os.path.join(args.images, src, f"{id}.png")
+        dest = os.path.join(dest_dir, f"{id}.png")
         shutil.copy(src, dest)
-
-    ImageIds = [f"{i:06}" for i in ImageIds]
+    
+    labels = [int(i) for i in labels]
     df = pd.DataFrame({"ImageId": ImageIds, "Label": labels})
     df.to_csv(f"./{args.set}/{args.set}-labels.csv", index=False)
 
@@ -48,3 +40,4 @@ def argument_parser():
 
 if __name__ == '__main__':
     argument_parser()
+
