94bb1e73be98d40a803c67a8a121417b471e1677
Mahmudulla Hassan <mhassan@miners.utep.edu>
2021-08-16 12:59:41 -0600

    add multiview option

--- xray/generate_dataset.py
+++ xray/generate_dataset.py
@@ -1,10 +1,10 @@
-import os
-import shutil
 from argparse import ArgumentParser
 from time import time
 
 import numpy as np
+import os
 import pandas as pd
+import shutil
 from tqdm import tqdm
 
 
@@ -12,32 +12,52 @@ def argument_parser():
     parser = ArgumentParser(description='Generate xray image dataset.')
     parser.add_argument('--images', type=str, required=True, action='store', help="Image directory")
     parser.add_argument('--set', type=str, required=True, action='store', help="Name of the set e.g., 'train'")
+    parser.add_argument('--multiview', action='store_true', default=False, help="Multi-view dataset")
     parser.add_argument('--split', type=float, required=True, action='store',
                         help="Ratio of positive to negative samples")
+    parser.add_argument('--output_dir', type=str, action='store', default='new-dataset',
+                        help="Output directory")
     args = parser.parse_args()
     tic = time()
 
-    dest_dir = f"./{args.set}/{args.set}-set"
-    if not os.path.isdir(dest_dir):
-        os.makedirs(dest_dir)
-
-    images = os.listdir(f"./{args.images}/no_ooi/zview")
+    images = os.listdir(os.path.join(args.images, f"{args.set}-set", "no_ooi", "zview"))
     ImageIds = [os.path.splitext(i)[0] for i in images]
     labels = np.random.uniform(size=len(ImageIds)) > args.split
 
-    for id, label in tqdm(zip(ImageIds, labels), total=len(labels)):
-        src = "ooi/zview" if label else "no_ooi/zview"
-        src = os.path.join(args.images, src, f"{id}.png")
-        dest = os.path.join(dest_dir, f"{id}.png")
-        shutil.copy(src, dest)
-    
+    if not args.multiview:
+        dest_dir = f"./{args.output_dir}/{args.set}-set"
+        if not os.path.isdir(dest_dir):
+            os.makedirs(dest_dir)
+
+        for id, label in tqdm(zip(ImageIds, labels), total=len(labels)):
+            src = "ooi/zview" if label else "no_ooi/zview"
+            src = os.path.join(args.images, src, f"{id}.png")
+            dest = os.path.join(dest_dir, f"{id}.png")
+            shutil.copy(src, dest)
+    else:
+        parent_dir = os.path.join(args.output_dir, f"{args.set}-set")
+        views = ['xview', 'yview', 'zview']
+        views_dirs = [os.path.join(parent_dir, v) for v in views]
+        # Create directories
+        for vdir in views_dirs:
+            if not os.path.isdir(vdir):
+                os.makedirs(vdir)
+            else:
+                raise IsADirectoryError("Old directory exists.")
+
+        for id, label in tqdm(zip(ImageIds, labels), total=len(labels)):
+            for view in views:
+                dest_dir = os.path.join(parent_dir, view)
+                src = f"ooi/{view}" if label else f"no_ooi/{view}"
+                src = os.path.join(args.images, f"{args.set}-set", src, f"{id}.png")
+                dest = os.path.join(dest_dir, f"{id}.png")
+                shutil.copy(src, dest)
+
     labels = [int(i) for i in labels]
     df = pd.DataFrame({"ImageId": ImageIds, "Label": labels})
-    df.to_csv(f"./{args.set}/{args.set}-labels.csv", index=False)
-
+    df.to_csv(os.path.join(args.output_dir, f"{args.set}-labels.csv"), index=False)
     print(f"Execution time: {time() - tic} seconds.")
 
 
 if __name__ == '__main__':
     argument_parser()
-
